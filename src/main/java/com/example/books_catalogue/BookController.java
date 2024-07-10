20c360c47efc6ae8bf243dbe388b70e46f340269
irfnhanif <14.irfanh@gmail.com>
2024-07-10 11:51:40 +0700

    feat: Update BookController to retrieve books with pagination and sorting

--- src/main/java/com/example/books_catalogue/BookController.java
+++ src/main/java/com/example/books_catalogue/BookController.java
@@ -2,6 +2,14 @@ package com.example.books_catalogue;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -15,8 +23,13 @@ public class BookController {
     }
     
     @GetMapping()
-    public String findAllBooks() {
-        return "Hello World";
+    public ResponseEntity<List<Book>> findAllBooks(Pageable pageable) {
+        Page<Book> page = bookRepository.findAll(PageRequest.of(
+            pageable.getPageNumber(),
+            pageable.getPageSize(),
+            pageable.getSortOr(Sort.by(Sort.Direction.ASC, "id"))
+        ));
+        return ResponseEntity.ok(page.getContent());
     }
     
 }
