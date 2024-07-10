a8c3120a60076faf5a143dae3693f7797fe0fd59
irfnhanif <14.irfanh@gmail.com>
2024-07-10 11:34:02 +0700

    Refactor BookRepository to extend PagingAndSortingRepository

--- src/main/java/com/example/books_catalogue/BookRepository.java
+++ src/main/java/com/example/books_catalogue/BookRepository.java
@@ -1,7 +1,10 @@
 package com.example.books_catalogue;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface BookRepository extends CrudRepository<Book, Integer> {
-    
+public interface BookRepository extends CrudRepository<Book, Integer>, PagingAndSortingRepository<Book, Integer> {
+    Page<Book> findAll(PageRequest pageRequest);
 }
