72ea100f2bb5a9a353c189c95a26d29c84789803
irfnhanif <14.irfanh@gmail.com>
2024-07-10 11:27:52 +0700

    feat: Refactor BookController to use BookRepository for retrieving books

--- src/main/java/com/example/books_catalogue/BookController.java
+++ src/main/java/com/example/books_catalogue/BookController.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
     
-    @GetMapping("")
+    @GetMapping()
     public String findAllBooks() {
         return "Hello World";
     }
