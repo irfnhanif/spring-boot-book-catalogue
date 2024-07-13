09b5af7c5cd597126ae1c301335a81fd0e8d4415
irfnhanif <14.irfanh@gmail.com>
2024-07-13 11:54:18 +0700

    Refactor BookRepository to use Pageable instead of PageRequest for findAll method

--- src/main/java/com/example/books_catalogue/BookRepository.java
+++ src/main/java/com/example/books_catalogue/BookRepository.java
@@ -2,12 +2,8 @@ package com.example.books_catalogue;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-
-import io.micrometer.common.lang.NonNull;
-import jakarta.annotation.Nonnull;
 
 public interface BookRepository extends CrudRepository<Book, Integer>, PagingAndSortingRepository<Book, Integer> {
     Page<Book> findAll(Pageable pageable);
