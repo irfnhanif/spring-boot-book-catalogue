63ec0f89360b277a710737f1c746f4fd50c72f6a
irfnhanif <14.irfanh@gmail.com>
2024-07-16 13:13:49 +0700

    feat: Add methods for checking and deleting books by ISBN

--- src/main/java/com/example/books_catalogue/BookRepository.java
+++ src/main/java/com/example/books_catalogue/BookRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface BookRepository extends CrudRepository<Book, Integer>, PagingAndSortingRepository<Book, Integer> {
     Page<Book> findAll(Pageable pageable);
     Book findByISBN(String ISBN);
+    boolean existsByISBN(String ISBN);
+    void deleteByISBN(String ISBN);
 }
